1ff4d57b96aa8bde901714cbf81e8cf10031e150
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-20 02:28:48 +0000

    Adding first version of paypal-donation-button
    git-svn-id: https://plugins.svn.wordpress.org/paypal-donation-button/trunk@1071465 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ paypal-donation-button.php
@@ -0,0 +1,263 @@
+<?php
+/*
+Plugin Name: PayPal Donation Button
+Plugin URI: https://wordpress.org/plugins/paypal-donation-button/
+Description: A simple PayPal donation button WordPress plugin.
+Version: 1.0.0
+Author: Sayful Islam
+Author URI: http://www.sayful.net
+License: GPLv2 or later
+*/
+
+// Activation Hook. Check if settings exists, if not register defaults.
+function paypal_donation_button_activation()
+{
+	$options_array = array(
+        'paypal_user_id' => get_option('admin_email'), 
+        'paypal_button' => 'large', 
+      	'currency' => 'USD', 
+    );
+	if ( get_option( 'paypal_donation_button_options' ) !== false ) {
+		// The option already exists, so we just update it.
+      	update_option( 'paypal_donation_button_options', $options_array );
+   } else{
+   		// The option hasn't been added yet. We'll add it with $autoload set to 'no'.
+   		add_option( 'paypal_donation_button_options', $options_array );
+   }
+}
+register_activation_hook( __FILE__, 'paypal_donation_button_activation' );
+
+// Adds  [paypal_donation_button] shortcode
+function paypal_donation_button_shortcode() {
+
+	$options = get_option('paypal_donation_button_options');
+
+	// Checks Which Image To Use
+	switch ($options['paypal_button']) {
+
+	    case 'small':
+	        $url = 'https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif';
+	        break;
+	    case 'medium':
+	        $url = 'https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif';
+	        break;
+	    case 'large':
+	        $url = 'https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif';
+	        break;
+	    default:
+	    	$url = 'https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif';
+
+	}
+
+	return '<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    			<div class="paypal_donation_button">
+			        <input type="hidden" name="cmd" value="_donations">
+			        <input type="hidden" name="business" value="'.$options['paypal_user_id'].'">
+			        <input type="hidden" name="rm" value="0">
+			        <input type="hidden" name="currency_code" value="'.$options['currency'].'">
+			        <input type="image" src="'.$url.'" name="submit" alt="PayPal - The safer, easier way to pay online.">
+			        <img alt="" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+			    </div>
+			</form>';
+};
+add_shortcode('paypal_donation_button','paypal_donation_button_shortcode');
+
+// To add shortcode in text widgets
+add_filter('widget_text', 'do_shortcode');
+
+function paypal_donation_button_custom_style() {
+	?>
+	<style type="text/css">
+		.paypal_donation_button:before,
+		.paypal_donation_button:after {
+		    content: " ";
+		    display: table;
+		}
+		.paypal_donation_button:after {
+			clear: both;
+		}
+		.paypal_donation_button {
+			max-width: 147px;
+			margin: 0 auto;
+			padding: 0;
+			display: block;
+		}
+	</style>
+	<?php
+}
+add_action('wp_head', 'paypal_donation_button_custom_style');
+
+
+function paypal_donation_button_callback() {
+	// Optional Callback
+}
+
+// Generate INPUT Field form form settings [EMAIL]
+function paypal_donation_button_user_id_callback() {
+	$options = get_option('paypal_donation_button_options');
+	echo "<input class='regular-text ltr' name='paypal_donation_button_options[paypal_user_id]' id='paypal_user_id' type='email' value='{$options['paypal_user_id']}'/>";
+}
+
+
+
+// Generate INPUT Field form form settings [RADIO]
+function paypal_donation_button_button_callback() {
+	$options = get_option('paypal_donation_button_options');
+	?>
+	<p>
+		<label>
+			<input type='radio' name='paypal_donation_button_options[paypal_button]' value='small' <?php if($options['paypal_button'] == 'small') { echo 'checked'; }  ?>>
+			<img src='https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif' alt='small' style='vertical-align: middle;margin-left: 15px;'>
+		</label>
+	</p>
+
+	<p>
+		<label>
+			<input type='radio' name='paypal_donation_button_options[paypal_button]' value='medium' <?php if($options['paypal_button'] == 'medium') { echo 'checked'; } ?>>
+			<img src='https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif' alt='medium' style='vertical-align: middle;margin-left: 15px;'>
+		</label>
+	</p>
+
+	<p>
+		<label>
+			<input type='radio' name='paypal_donation_button_options[paypal_button]' value='large' <?php if($options['paypal_button'] == 'large') { echo 'checked'; } ?>>
+			<img src='https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif' alt='large' style='vertical-align: middle;margin-left: 15px;'></br>
+		</label>
+	</p>
+
+	<?php
+}
+
+
+
+// Generate INPUT Field form form settings [DROPDOWN]
+function paypal_donation_button_currency_callback() {
+	$options = get_option('paypal_donation_button_options');
+
+	$currency = array(
+					'AUD' => 'Australian Dollars (A $)',
+					'BRL' => 'Brazilian Real',
+					'CAD' => 'Canadian Dollars (C $)',
+					'CZK' => 'Czech Koruna',
+					'DKK' => 'Danish Krone',
+					'EUR' => 'Euros (€)',
+					'HKD' => 'Hong Kong Dollar ($)',
+					'HUF' => 'Hungarian Forint',
+					'ILS' => 'Israeli New Shekel',
+					'JPY' => 'Yen (¥)',
+					'MYR' => 'Malaysian Ringgit',
+					'MXN' => 'Mexican Peso',
+					'NOK' => 'Norwegian Krone',
+					'NZD' => 'New Zealand Dollar ($)',
+					'PHP' => 'Philippine Peso',
+					'PLN' => 'Polish Zloty',
+					'GBP' => 'Pounds Sterling (£)',
+					'RUB' => 'Russian Ruble',
+					'SGD' => 'Singapore Dollar ($)',
+					'SEK' => 'Swedish Krona',
+					'CHF' => 'Swiss Franc',
+					'TWD' => 'Taiwan New Dollar',
+					'THB' => 'Thai Baht',
+					'TRY' => 'Turkish Lira',
+					'USD' => 'U.S. Dollars ($)',
+	 			);
+	?>
+	<select id='currency_code' name='paypal_donation_button_options[currency]'>
+		<?php
+			foreach($currency as $code => $label) :
+				if( $code == $options['currency'] ) { $selected = "selected='selected'"; } else { $selected = ''; }
+				echo "<option {$selected} value='{$code}'>{$label}</option>";
+			endforeach;	
+		?>
+	</select>
+	<?php
+}
+
+
+
+// Register All Settings And And Setting Fields as Used in wordpress
+function paypal_donation_button_settings_and_fields() {
+
+	// $option_group, $option_name, $sanitize_callback
+	register_setting(
+		'paypal_donation_button_options',
+		'paypal_donation_button_options'
+	);
+
+	// $id, $title, $callback, $page
+	add_settings_section(
+		'donate_plugin_main_section',
+		__('Main Settings'),
+		'paypal_donation_button_callback',
+		__FILE__
+	);
+
+	// $id, $title, $callback, $page, $section, $args
+	add_settings_field(
+		'paypal_user_id', 
+		__('PayPal ID:'), 
+		'paypal_donation_button_user_id_callback', 
+		__FILE__, 
+		'donate_plugin_main_section',
+		array( 'label_for' => 'paypal_user_id' )
+	);
+
+	// $id, $title, $callback, $page, $section, $args
+	add_settings_field(
+		'paypal_button', 
+		__('Choose Donate Button:'), 
+		'paypal_donation_button_button_callback', 
+		__FILE__, 
+		'donate_plugin_main_section',
+		array( 'label_for' => 'paypal_button' )
+	);
+
+	// $id, $title, $callback, $page, $section, $args
+	add_settings_field(
+		'currency', 
+		__('Select Currency:'), 
+		'paypal_donation_button_currency_callback', 
+		__FILE__, 
+		'donate_plugin_main_section',
+		array( 'label_for' => 'currency' )
+	);
+}
+
+add_action('admin_init', 'paypal_donation_button_settings_and_fields');
+
+
+// Admin Menu Action Hook
+function paypal_donation_button_options_init() {
+
+	// page_title,  menu_title, capability, menu_slug, function
+	add_options_page(
+		__('PayPal Donation'),
+		__('PayPal Donation'),
+		'administrator',
+		__FILE__,
+		'paypal_donation_button_options_page'
+	);
+}
+add_action('admin_menu', 'paypal_donation_button_options_init');
+
+// Generate HTML of main options page
+function paypal_donation_button_options_page() {
+
+	?>
+		<div class="wrap">
+    		<h2><?php _e('PayPal Donation Button Settings') ?></h2>
+			<form method="post" action="options.php" enctype="multipart/form-data">
+			<?php 
+				// $option_group
+				settings_fields( 'paypal_donation_button_options' );
+
+				// $page 
+				do_settings_sections( __FILE__ );
+
+				// Submit Button
+				submit_button();
+			?>
+			</form>
+		</div>
+	<?php
+}
---
+++ readme.txt
@@ -0,0 +1,71 @@
+=== PayPal Donation Button ===
+Contributors: sayful
+Tags: PayPal, Donation, Donate, PayPal Donate,
+Requires at least: 3.0
+Tested up to: 4.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A simple PayPal donation button WordPress plugin.
+== Description ==
+
+A simple PayPal donation button WordPress plugin.
+
+= Usage =
+
+After installing and Activating the plugin, go to WordPress Admin Dahsbord and then go to
+
+`Settings >> PayPal Donation`
+
+Here you can change default settings. By default your WordPress site admin email will be used as PayPal ID. So if you have different PayPal ID, change it here. Also choose donate button size and select your desired currency.
+
+After saving PayPal Donation Settings, just paste the following shortcode at any page or text widget where your want to show "PayPal Donation Button"
+
+`[paypal_donation_button]`
+
+== Installation ==
+Installing the plugins is just like installing other WordPress plugins. If you don't know how to install plugins, please review the two options below:
+
+Install by Search
+
+* From your WordPress dashboard, choose 'Add New' under the 'Plugins' category.
+* Search for 'PayPal Donation Button' a plugin will come called 'PayPal Donation Button' and Click 'Install Now' and confirm your installation by clicking 'ok'
+* The plugin will download and install. Just click 'Activate Plugin' to activate it.
+
+Install by ZIP File
+
+* From your WordPress dashboard, choose 'Add New' under the 'Plugins' category.
+* Select 'Upload' from the set of links at the top of the page (the second link)
+* From here, browse for the zip file included in your plugin titled 'paypal-donation-button.zip' and click the 'Install Now' button
+* Once installation is complete, activate the plugin to enable its features.
+
+Install by FTP
+
+* Find the directory titles 'paypal-donation-button' and upload it and all files within to the plugins directory of your WordPress install (WORDPRESS-DIRECTORY/wp-content/plugins/) [e.g. www.yourdomain.com/wp-content/plugins/]
+* From your WordPress dashboard, choose 'Installed Plugins' option under the 'Plugins' category
+* Locate the newly added plugin and click on the \'Activate\' link to enable its features.
+
+
+== Frequently Asked Questions ==
+Do you have questions or issues with PayPal Donation Button? [Ask for support here.](http://wordpress.org/support/plugin/paypal-donation-button)
+
+== Screenshots ==
+1. Screenshot of option page of PayPal Donation Button
+2. Screenshot of PayPal Donation Button Shortcode (Text Widget)
+3. Screenshot of display of PayPal Donation Button (Text Widget)
+4. Screenshot of PayPal Donation Button Shortcode (Page)
+5. Screenshot of display of PayPal Donation Button (Page)
+
+== Changelog ==
+
+
+= 1.0 =
+* Utilizing basic functionality
+
+== Upgrade Notice ==
+You should upgrade the plugin to get more function and faster speed.
+
+== CREDIT ==
+
+1. This plugin was developed by [Sayful Islam](http://sayful.net)
